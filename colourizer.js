a422f05acdba435adad38500148026ef57fb4ed1
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-04-25 22:39:13 -0400

    Actually applies colors to page, adding jQuery

--- colourizer.js
+++ colourizer.js
@@ -4,9 +4,37 @@ addedElement.appendChild(document.createTextNode('Hello, world!'));
 var body = document.body;
 body.insertBefore(addedElement, body.firstChild);
 
+var page_colourizer = {
+  random_palette_url: 'http://www.colourlovers.com/api/palettes/random',
+
+  load_random_palette: function(callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", this.random_palette_url, true);
+    req.onload = function(e) {
+      this.get_hex_codes(e, callback);
+    }.bind(this);
+    req.send(null);
+  },
+
+  get_hex_codes: function(e, callback) {
+    var hex_nodes = e.target.responseXML.querySelectorAll('hex');
+    var hex_codes = [];
+    for (var i=0; i<hex_nodes.length; i++) {
+      hex_codes[i] = '#' + hex_nodes[i].textContent;
+    }
+    console.log(hex_codes);
+    callback(hex_codes);
+  }
+};
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  if (request.greeting == "hello") {
-    sendResponse({farewell: 'what is this'});
+  if (request.greeting == "load_random_palette") {
+    page_colourizer.load_random_palette(function(hex_codes) {
+      sendResponse({hex_codes: hex_codes});
+      $('body').css('background-color', hex_codes[0], 'important');
+      $('body, p, li, span, th, td, table, caption, legend').css('color', hex_codes[1], 'important');
+      $('a').css('color', hex_codes[2], 'important');
+    });
   } else {
     sendResponse({});
   }
