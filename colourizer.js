74621989a735fddce0f57caa4e2c03bf7f0f84f7
Sarah Vessels <cheshire137@gmail.com>
2013-04-25 23:19:58 -0400

    Displaying palette title in popup

--- colourizer.js
+++ colourizer.js
@@ -5,26 +5,28 @@ var page_colourizer = {
     var req = new XMLHttpRequest();
     req.open("GET", this.random_palette_url, true);
     req.onload = function(e) {
-      this.get_hex_codes(e, callback);
+      this.get_palette_data(e, callback);
     }.bind(this);
     req.send(null);
   },
 
-  get_hex_codes: function(e, callback) {
-    var hex_nodes = e.target.responseXML.querySelectorAll('hex');
+  get_palette_data: function(e, callback) {
+    var xml = e.target.responseXML;
+    var hex_nodes = xml.querySelectorAll('hex');
+    var title = xml.querySelector('title').textContent;
     var hex_codes = [];
     for (var i=0; i<hex_nodes.length; i++) {
       hex_codes[i] = '#' + hex_nodes[i].textContent;
     }
-    console.log(hex_codes);
-    callback(hex_codes);
+    callback({hex_codes: hex_codes, title: title});
   }
 };
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.greeting == "load_random_palette") {
-    page_colourizer.load_random_palette(function(hex_codes) {
-      sendResponse({hex_codes: hex_codes});
+    page_colourizer.load_random_palette(function(palette_data) {
+      sendResponse(palette_data);
+      var hex_codes = palette_data.hex_codes;
       $('body').css('background-color', hex_codes[0], 'important');
       $('body, p, li, span, th, td, table, caption, legend').css('color', hex_codes[1], 'important');
       $('a').css('color', hex_codes[2], 'important');
--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendRequest(tab.id, {greeting: "load_random_palette"}, function(response) {
-      var hex_codes = response.hex_codes;
+    chrome.tabs.sendRequest(tab.id, {greeting: "load_random_palette"}, function(palette_data) {
+      var hex_codes = palette_data.hex_codes;
+      var h1 = document.createElement('h1');
+      h1.appendChild(document.createTextNode(palette_data.title));
+      document.body.appendChild(h1);
       for (var i = 0; i < hex_codes.length; i++) {
         var div = document.createElement('div');
         div.style.backgroundColor = hex_codes[i];
