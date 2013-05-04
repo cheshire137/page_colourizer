850c11c04632686ed68b90541442e7cdc4553d00
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-05-04 00:29:23 -0400

    Fixing popup causing new palette in Colour Frenzy mode

--- extension/background.js
+++ extension/background.js
@@ -4,17 +4,7 @@ chrome.tabs.onUpdated.addListener(function(tab_id, change_info) {
       tab_id,
       {greeting: 'tab_updated', tab_id: tab_id},
       function() {
-        // We got a response, so Colour Frenzy mode must be enabled. Go ahead
-        // and remove any stored data for tabs that no longer exist.
-        chrome.tabs.getAllInWindow(null, function(tabs) {
-          chrome.tabs.sendRequest(
-            tab_id,
-            {greeting: 'garbage_collect', tabs: tabs},
-            function() {
-              // no-op
-            }
-          );
-        });
+        // no-op
       }
     );
   }
